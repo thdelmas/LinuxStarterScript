c03bdeb69715f071110ad5e41a4541f30e92c9de
morph33 <morph33@rocket.rocket>
2019-09-29 13:00:31 +0200

    ADD bashrc sample

--- lnx_starter.sh
+++ lnx_starter.sh
@@ -6,6 +6,12 @@
 main()
 {
 	echo	"Linux Starter Script"
+	echo	"Setup Apt repository"
+	echo	"Install packages"
+	echo	"Configure .dot"
+	echo	"Configure UX (gnome, Unity ...)"
+	echo	"Run followings command  lines"
+	echo	"Hack with love, T.H.D."
 }
 
 main
