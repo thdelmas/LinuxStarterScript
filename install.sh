5f608eea30aaf490d4ab0ca71ac664cbcac1a120
Jerry <jerry@dump-accueil-1.42.fr>
2019-11-08 16:58:22 +0100

    ADD add_cd_path

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ git_prompt () {
 }
 
 px_set_prompt () {
-RESET='\033[0m'
-export PS1="\u@\h|\w [$?] "
-if [ "$(git status | wc -l)" == '4' ]
+	RESET='\033[0m'
+	export PS1="\u@\h|\w [$?]"
+	if [ "$(git status | wc -l)" -eq '4' ]
 	then
-	export PS1="$PS1(\033[32m`git_prompt`$RESET)\n> "
+		export PS1="$PS1\(\033[32m`git_prompt`$RESET\)\n> "
 	else
-	export PS1="$PS1(\033[31m`git_prompt`$RESET)\n> "
+		export PS1="$PS1\(\033[31m`git_prompt`$RESET\)\n> "
 	fi
 }
 
@@ -29,21 +29,27 @@ px_choose_shell () {
 
 }
 
+add_cd_path () {
+	for i in $@
+	do
+		if [ ! "$(echo $CDPATH | tr ':' '\n' | grep \'$i'$'\')" ]
+		then
+			export CDPATH="${CDPATH:+$CDPATH:}$i"
+		fi
+	done
+}
+
 cat <<-EOF
 
 
- ██▓███   ██▓ ███▄ ▄███▓ ██▓███     ▒██   ██▒
-▓██░  ██▒▓██▒▓██▒▀█▀ ██▒▓██░  ██▒   ▒▒ █ █ ▒░
-▓██░ ██▓▒▒██▒▓██    ▓██░▓██░ ██▓▒   ░░  █   ░
-▒██▄█▓▒ ▒░██░▒██    ▒██ ▒██▄█▓▒ ▒    ░ █ █ ▒
-▒██▒ ░  ░░██░▒██▒   ░██▒▒██▒ ░  ░   ▒██▒ ▒██▒
-▒▓▒░ ░  ░░▓  ░ ▒░   ░  ░▒▓▒░ ░  ░   ▒▒ ░ ░▓ ░
-░▒ ░      ▒ ░░  ░      ░░▒ ░        ░░   ░▒ ░
-░░        ▒ ░░      ░   ░░           ░    ░
-	  ░         ░                ░    ░
+██████╗ ██╗███╗   ███╗██████╗     ██╗  ██╗
+██╔══██╗██║████╗ ████║██╔══██╗    ╚██╗██╔╝
+██████╔╝██║██╔████╔██║██████╔╝     ╚███╔╝ 
+██╔═══╝ ██║██║╚██╔╝██║██╔═══╝      ██╔██╗ 
+██║     ██║██║ ╚═╝ ██║██║         ██╔╝ ██╗
+╚═╝     ╚═╝╚═╝     ╚═╝╚═╝         ╚═╝  ╚═╝
 
+                                          
 EOF
-
-px_set_prompt
 
 echo "PimpX: Installation ended"
