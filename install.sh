3ec6491f095023bbff413621b0173e24ca2f76bf
jerry gauler <jerry@dump-accueil-2.42.fr>
2020-02-06 16:41:23 +0100

    refactor (install.sh): rewrite install whereis

--- install.sh
+++ install.sh
@@ -1,97 +1,17 @@
-git_prompt () {
-	OK='\033[32m' ;
-	KO='\033[31m' ;
-	GIT_STATUS="$( git status | grep 'Your branch is up to date' )" ;
-	if [ "$GIT_STATUS" ]
-		then
-			echo "$OK`git branch -a | cat | grep '\*' | cut -d' ' -f2`" ;
-	else
-		echo "$KO`git branch -a | cat | grep '\*' | cut -d' ' -f2`" ;
-	fi
-}
+#!/bin/bash 
 
-px_set_prompt () {
-	RESET='\033[0m'
-	export PS1="\u@\h|\w [$?]"
-	if [ "$(git status | wc -l)" -eq '4' ]
-	then
-		export PS1="$PS1(\033[32m`git_prompt`$RESET)\n> "
-	else
-		export PS1="$PS1(\033[31m`git_prompt`$RESET)\n> "
-	fi
-}
+##################################################################
+#Script Name	: pimpx.sh
+#Description	: 
+#Args          	:
+#Author       	: Thdelmas
+#Email         	: thedelleg@gmail.com
+###################################################################
 
-px_choose_shell () {
+PIMPX_VERSION=1.0.0
 
-	SHELL_OK=`cat /etc/shells | sed -e '/^#/d' | sed -e '/^$/d'` ;
-	echo "Your current shell: $SHELL" ;
-
-	select var in $SHELL_OK
-		do
-			if [ "$var" ]
-				then
-					echo "You choose $var" ;
-		break ;
-		fi
-			done ;
-}
-
-add_cd_path () {
-	for i in $@
-	do
-		if [ ! "$(echo $CDPATH | tr ':' '\n' | grep \'$i'$'\')" ]
-		then
-			export CDPATH="${CDPATH:+$CDPATH:}$i"
-		fi
-	done
-}
-
-pimpx_set_xdg () {
-	if [ ! "$XDG_DATA_HOME" ]
-	then
-		export XDG_DATA_HOME="$HOME/.local/share"
-		mkdir -p "$XDG_DATA_HOME"
-		chmod 0700 "$XDG_DATA_HOME"
-	fi
-	if [ ! "$XDG_CONFIG_HOME" ]
-	then
-		export XDG_CONFIG_HOME="$HOME/.config"
-		mkdir -p "$XDG_CONFIG_HOME"
-		chmod 0700 "$XDG_CONFIG_HOME"
-	fi
-	if [ ! "$XDG_DATA_DIRS" ]
-	then
-		export XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
-	fi
-	if [ ! "$XDG_CONFIG_DIRS" ]
-	then
-		export XDG_CONFIG_DIRS="/etc/xdg"
-	fi
-	if [ ! "$XDG_CACHE_HOME" ]
-	then
-		export XDG_CACHE_HOME="$HOME/.cache"
-		mkdir -p "$XDG_CACHE_HOME"
-		chmod 0700 "$XDG_CACHE_HOME"
-	fi
-}
-
-pimpx_launch () {
-	pimpx_set_xdg
-}
-
-pimpx_rc () {
-	PIMPXRC="$PIMPX/pimpx.rc"
-	echo '#- Pimp X Configuration file -#' > "$PIMPXRC"
-	echo "PIMPX='$PIMPX'" >> "$PIMPXRC"
-	echo '#- Load pimpx functions in pimpx/functions -#' >> "$PIMPXRC"
-	echo "for i in \`ls $PIMPX/functions\` ;\n do ;\n source \"$PIMPX/functions/\$i\" ;\n done" >> "$PIMPXRC"
-}
-
-
-
-pimpx_install () {
+pimpx_hello () {
 	cat <<-EOF
-
 
 ██████╗ ██╗███╗   ███╗██████╗     ██╗  ██╗
 ██╔══██╗██║████╗ ████║██╔══██╗    ╚██╗██╔╝
@@ -100,41 +20,77 @@ pimpx_install () {
 ██║     ██║██║ ╚═╝ ██║██║         ██╔╝ ██╗
 ╚═╝     ╚═╝╚═╝     ╚═╝╚═╝         ╚═╝  ╚═╝
 
-
 EOF
-echo "## \/ Pimp X uninstall function \/ ##
+}
 
-pimpx_uninstall () {
-	echo 'Good Bye, friend..\n See you soon.'
-	rm -rf '$PIMPX'
-	echo 'BYBYEE'
-}" > "$PIMPX/functions/pimpx_unistall.sh"
-
-
-echo "Default shell: $SHELL"
-pimpx_set_xdg
-export PIMPX="$XDG_CONFIG_HOME/pimpx"
-
-if [ -d "$PIMPX" ]
-then
-	echo "Pimpx already installed :)"
-	read -p "Update ? (y/n): " stdin
-	if [ "$stdin"  = 'y' ]
+pimpx_whereis() {
+	if [ "$XDG_CONFIG_HOME" -a -w "$XDG_CONFIG_HOME" -a -r "$XDG_CONFIG_HOME" ]
 	then
-		cd "$PIMPX" && git pull && echo "Now up-to-date :)"
+		export PIMPX="$XDG_CONFIG_HOME/pimpx"
+	elif [ "$HOME" -a -w "$HOME/.config" -a -r "$HOME/.config" ]
+	then
+		export PIMPX="$HOME/.config/pimpx"
+	elif [ -w '/etc' -a -r '/etc' ]
+	then
+		export PIMPX='/etc/pimpx'
+	elif [ "$HOME" -a -w "$HOME" -a -r "$HOME" ]
+	then
+		export PIMPX="$HOME/.pimpx"
+	else
+		return 1
 	fi
-else
-	echo "Installing PimpX here: $PIMPX"
-	if [ "$(git --version | grep version)" ]
-	then 
-		git clone https://github.com/thdelmas/pimpx "$PIMPX"
+	return 0
+}
+
+pimpx_recover() {
+	if [ "$PIMPX" ]
+	then
+		cd "$PIMPX" &&
+			rm -rf "$PIMPX/core"
+					curl https://gitlab.com/thdelmas/PimpX/-/archive/master/PimpX-master.tar.gz | gunzip | tar -xv && mv "$PIMPX/Pimpx-master" "$PIMPX/core"
 	fi
-	pimpx_rc
-	echo "PimpX is now installed"
-fi
+}
 
+pimpx_update() {
+	if [ ! -f "$PIMPX/version" ] || [ "$(cat "$PIMPX/version" | sed 's/\.//g')" -lt "$(echo "$PIMPX_VERSION" | sed 's/\.//g')" ]
+	then
+		echo "Update ? (y/n)"
+		read rep
+		if [ "$rep" = 'Y' -o "$rep" = 'y' -o "$rep" = 'yes' ]
+		then
+			echo 'Update . . .'
+			pimpx_recover
+		fi
+	fi
+}
 
+pimpx_install() {
+	pimpx_hello
+	if [ ! "$PIMPX" ]
+	then
+		if ! pimpx_whereis
+		then
+			echo 'Failed to install Pimpx'
+		else
+			echo "Pimpx installation path: $PIMPX"
+		fi
+	fi
+	if [ "$PIMPX" -a ! -d "$PIMPX" ]
+	then
+		echo 'Install . . .'
+		rm -rf "$PIMPX"
+		mkdir "$PIMPX"
+		cd "$PIMPX"
+		mkdir "$PIMPX/functions" "$PIMPX/scripts"
+		rm -rf "$PIMPX/core"
+		curl https://gitlab.com/thdelmas/PimpX/-/archive/master/PimpX-master.tar.gz | gunzip | tar -xv && mv "$PIMPX/Pimpx-master" "$PIMPX/core"
+	elif [ "$PIMPX" -a -d "$PIMPX" ]
+	then
+		echo 'Pimpx is already installed'
+		pimpx_update
+	else
+		echo 'Pimpx not installed'
+	fi
 }
 
 pimpx_install
-
